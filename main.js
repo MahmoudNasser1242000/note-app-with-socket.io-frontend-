2b7b0cb098afd125625e277eb3dcd2f3d459fc18
MahmoudNasser1242000 <mahmoudahmed102030405060@gmail.com>
2024-09-11 18:35:12 +0300

    error validation

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const note_title = document.querySelector("#note-title");
 const note_desc = document.querySelector("#note-desc");
 const form_btn = document.querySelector("#add-note");
 
+const title_validation = document.querySelector("#title-validation");
+const description_validation = document.querySelector("#description-validation");
+
+let clear = true
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
@@ -12,22 +17,24 @@ socket.on("connect", () => {
     socket.emit("load")
 })
 
+// add notes
 form_btn.addEventListener("click", (e) => {
-    e.preventDefault()
     const note_data = {
         title: note_title.value,
         description: note_desc.value
     }
     socket.emit("add-note", note_data)
-
-    note_title.value = ""
-    note_desc.value = ""
 })
 
+
+// display notes
 socket.on("display-notes", (notes) => {
     display_notes(notes)
 })
 const display_notes = (notes) => {
+    clearInputs()
+    clearValidationErrors()
+
     let cartona = ``
     notes.forEach((note) => {
         cartona += `
@@ -46,6 +53,43 @@ const display_notes = (notes) => {
     })
     notes_container.innerHTML = cartona;
 }
+
+const clearInputs = () => {
+    note_title.value = ""
+    note_desc.value = ""
+}
+
+// delete notes
 const delete_notes = (id) => {
     socket.emit("delete-note", id)
 }
+
+
+// validation error
+const clearValidationErrors = () => {
+    title_validation.classList.remove("d-block")
+    title_validation.innerHTML = ""
+
+    description_validation.classList.remove("d-block")
+    description_validation.innerHTML = ""
+}
+
+socket.on("error-validation", (errValidation) => {
+    clear = false;
+
+    if (errValidation?.title) {
+        title_validation.classList.add("d-block");
+        title_validation.innerHTML = errValidation?.title;
+    } else {
+        title_validation.classList.remove("d-block");
+        title_validation.innerHTML = "";
+    }
+
+    if (errValidation?.description) {
+        description_validation.classList.add("d-block");
+        description_validation.innerHTML = errValidation?.description;
+    } else {
+        description_validation.classList.remove("d-block");
+        description_validation.innerHTML = "";
+    }
+})
